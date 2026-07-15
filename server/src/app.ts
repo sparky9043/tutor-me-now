4f7ce3a1f5a51469b739399ed740b46cdfabb56b
Se Joon Park <sparky9043@gmail.com>
2026-07-15 16:54:48 +0900

    initialize node env for dev, production and test

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,7 +4,10 @@ import morgan from 'morgan';
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
