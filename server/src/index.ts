343e7147a7816378408a2583f60bdc62d0e1b027
Se Joon Park <sparky9043@gmail.com>
2026-07-15 16:49:27 +0900

    replace console.log with logger function

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import app from "./app.ts";
 import config from "./utils/config.ts";
+import logger from "./utils/logger.ts";
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`);
+  logger.info(`Server running on port ${config.PORT}`);
 });
